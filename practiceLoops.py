0c13da37a02ae43c49ae3ef2a7372033768c9847
erikajimenez <55327070+erikajimenez@users.noreply.github.com>
2019-11-02 12:57:07 -0500

    Initial commit

---
+++ practiceLoops.py
@@ -0,0 +1,18 @@
+i = 2
+while i  < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+    
+fruits = ["apple", "blueberry", "raspberry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "blueberry":
+        break
+    
+n = 5
+for i in range(n):
+    for j in range(i):
+        print("* ", end = "")
+        print("")
